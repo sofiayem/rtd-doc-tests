ea239921e4035280cecc8b2ba47a11e75b58a5e3
Sofia Yemelianova <yemelianova407@gmail.com>
2019-05-15 16:14:25 +0300

    minor fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = 'imgs/Symbol@2x.png'
+html_logo = 'imgs/Symbol.png'
 
 html_theme_options = {
     'display_version': True,
